6a8fb6ae0f8f5ef436414ee25561f6407ab09c2d
nulysses <adr.zivil@gmail.com>
2021-07-06 22:22:56 +0200

    1

--- install.sh
+++ install.sh
@@ -32,18 +32,19 @@ set -o pipefail
 set -eu
 #################################SCRIPT_START##################################
 
+ar18.script.import ar18.script.install
+ar18.script.import ar18.script.read_target
+ar18.script.import ar18.script.execute_with_sudo
+
 . "${script_dir}/vars"
-if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
-obtain_sudo_password
 
-
-ar18_install "${install_dir}" "${module_name}" "${script_dir}" 
+ar18.script.install "${install_dir}" "${module_name}" "${script_dir}"
 
 set +u
-ar18_deployment_target="$(read_target "${1}")"
+ar18_deployment_target="$(ar18.script.read_target "${1}")"
 set -u
 
-echo "${ar18_sudo_password}" | sudo -Sk chmod +x "${script_dir}/setup_dropbear/setup_dropbear.sh"
+ar18.script.execute_with_sudo chmod +x "${script_dir}/setup_dropbear/setup_dropbear.sh"
 
 "${script_dir}/setup_dropbear/setup_dropbear.sh" "${ar18_deployment_target}"
 
